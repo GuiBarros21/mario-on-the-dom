07a0b7b608a56fe997e1c30ee5e6427da2590bb5
Guilherme Barros <barros.guilherme21@gmail.com>
2022-01-26 12:17:04 -0800

    Result
    It is the result of the work

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,90 @@ function isTouching(a, b) {
     aRect.left > bRect.left + bRect.width
   );
 }
+
+
+
+
+const coin = document.querySelector('#coin');
+const mario = document.querySelector('#avatar');
+
+// It fixed my problem
+mario.style.left = 100 +'px';
+mario.style.top =  100 + 'px';
+
+
+
+let score = 0; 
+
+function calcScore()
+{
+//insert score on the screen
+
+const scoreCountEl = document.querySelector('#score')
+scoreCountEl.innerHTML = `Total score : ${score}`;
+document.body.insertBefore(scoreCountEl,coin);
+score+= 1;
+}
+moveCoin();
+calcScore();
+
+function moveCoin()
+{
+
+  let radomPlaceX = Math.floor(Math.random() *1000);
+  let radomPlaceY = Math.floor(Math.random() *800);
+
+ 
+  coin.style.left = radomPlaceX +'px';
+  coin.style.top =  radomPlaceY + 'px';
+}
+
+//function that moves mario
+
+const moveJump = 50;
+
+document.addEventListener("keydown", moveImage,false);
+
+function moveImage(e) {
+    let kc = e.keyCode;
+    
+    var top = parseInt(mario.style.top.substr(0,mario.style.top.length - 2 ));
+    //Gets the current "left" value minus the "px" as an int.
+    var left = parseInt(mario.style.left.substr(0, mario.style.left.length - 2));
+
+    
+
+    switch (kc) {
+        case 37:
+            //Pressed left.
+            mario.style.left = (left - moveJump) +'px';
+            
+            break;
+        case 38:
+            //Pressed up.
+            mario.style.top = (top - moveJump)  + 'px';
+            break;
+        case 39:
+            //Pressed right.
+            mario.style.left = (left + moveJump) + 'px';
+            break;
+        case 40:
+            //Pressed down.
+            mario.style.top = (top + moveJump) + 'px';
+            break;
+    }
+  //check if mario got the coin
+   if (isTouching(mario,coin) ==true)
+   {
+     console.log(score);
+     moveCoin();
+     calcScore();
+     
+   }
+   
+    
+}
+
+
+
+
